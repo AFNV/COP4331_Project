a9c875fa51e7264e8f8585e0334c8fa14bd5e6af
FXRevolution <42753102+FXRevolution@users.noreply.github.com>
2018-11-05 19:26:11 -0500

    Update Transmitter.ino

--- Code/Sprint-3/Transmitter.ino
+++ Code/Sprint-3/Transmitter.ino
@@ -3,8 +3,11 @@
 #include "LiquidCrystal.h"
 //Variables
 const int buttonPin1 = 2; // button Read 
-const int z =0;
+// const int buttonPin2 = x;
+
+const int z = 0;
 int buttonR = 0; //initial value of Read button
+int buttonW = 0;
 const int rs = 12, en = 11, d4 = 6, d5 = 5, d6 = 4, d7 = 3;
 LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
 SignalRead r;
@@ -12,6 +15,8 @@ SignalRead r;
 
 void setup() {
     pinMode(buttonPin1,INPUT);
+    // pinMode(buttonPin2, INPUT);
+    
     lcd.begin(16, 2);
     Serial.begin(9600); //sets serial port for communication
 }
@@ -21,12 +26,16 @@ void setup() {
  * Button functionality
  */
 void loop() {
-  
     buttonR = digitalRead(buttonPin1);
-    if(buttonR == HIGH){
+    //buttonW = digitalRead(buttonPin2);
+    
+    if(buttonR == HIGH)
+    {
      r.readLight();
      lcd.print(r.getDefinition(r.inputString));
-    }else{
+    }
+    else
+    {
      return z;
     }
 
