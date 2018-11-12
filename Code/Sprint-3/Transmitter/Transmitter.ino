d812a82bf7c84346ced30d42e899cd277a237b29
FXRevolution <42753102+FXRevolution@users.noreply.github.com>
2018-11-11 22:20:52 -0500

    Add files via upload

---
+++ Code/Sprint-3/Transmitter/Transmitter.ino
@@ -0,0 +1,91 @@
+/*OPT3001_Backlight Example
+In this example, we will read LUX readings from the OPT3001 and will use that to
+modulate a PWM that controls an LED backlight to an LCD. LUX readings are also sent
+to the serial monitor.
+
+Created by Adrian Fernandez & Dung Dang, Dec 2013
+Released to public domain.
+*/
+
+#include <Wire.h>			// Needed by Energia for Tiva C LaunchPad 
+#include "OPT3001.h"
+#include "signalread.h"
+
+#define USE_USCI_B1 
+opt3001 opt3001;
+
+SignalRead* r;
+SignalWrite* w;
+
+const uint8_t ledGreen =  38;      // the number of the LED pin
+const uint8_t ledBlue =  37;      // the number of the LED pin
+const uint8_t whiteLED =  18;
+const uint8_t button =  33;
+unsigned long reading;
+
+int buttonOneState = 0; 
+
+char text[] = "hello world";
+
+
+
+void setup()
+{
+  // initialize the LED pins as  output:
+  pinMode(ledGreen, OUTPUT);     
+  pinMode(ledBlue, OUTPUT); 
+  pinMode(whiteLED, OUTPUT);  
+  pinMode(button, INPUT_PULLUP);  
+
+  digitalWrite(whiteLED, LOW);
+  digitalWrite(ledBlue, LOW);
+  digitalWrite(ledGreen, LOW);
+
+  
+ 
+  Serial.begin(115200);
+  delay(1000); 
+  opt3001.begin(); 
+  Serial.println("OPT3001 Initialized!");
+
+  r = new SignalRead();
+  w = new SignalWrite(37);  
+}
+
+void loop()
+{
+  buttonOneState = digitalRead(button);
+  if(buttonOneState == LOW)
+  {
+ 
+  w->codeText(text, sizeof(text));
+  
+  /* reading = opt3001.readResult();
+  //Serial.println(SensorValue);//output value
+  if(reading > 4300)// calculating the number of dots and dashes
+  {
+    digitalWrite(ledBlue, HIGH);
+    delay(200);
+    digitalWrite(ledBlue, LOW);
+    delay(200);
+    
+    for(int i=0;i<=13;i++)
+    {
+      reading=opt3001.readResult();
+     
+      r->readLight(reading, i);
+    
+    }
+    digitalWrite(ledBlue, LOW);
+
+     r->printChar();
+   r->resetString();
+
+  }
+     */
+  
+  }
+
+}
+
+
---
+++ Code/Sprint-3/Transmitter/signalread.h
@@ -0,0 +1,160 @@
+/*
+Code for the Phototransister: Captures lights and turns it into Letters
+*/
+#include "signalwrite.h"
+
+
+class SignalRead
+{
+
+  public:
+    SignalRead()
+    {
+       for(int i=0; i<=13; i++)
+         inputString[i]=0;
+       inputChar ='_';
+       ledBlue = 37;
+    }
+
+    ~SignalRead(){}
+    
+    char getChar()
+    {
+      return inputChar;
+    }
+    
+    void readLight(unsigned long value, int index);
+    
+    void printChar()
+    {
+      inputChar = getDefinition(inputString);
+      Serial.print(inputChar);
+    }
+    
+    void resetString()
+    {
+      for(int i=0; i<=13; i++)
+         inputString[i]=0;
+    }
+    
+    char getDefinition(char s[]);
+    
+    bool strcomp(char a[], char b[]);
+
+  private:
+    char inputChar;
+    char inputString[14];
+    uint8_t ledBlue;      // the number of the LED pin
+};
+
+
+
+
+void SignalRead::readLight(unsigned long value, int index)
+{
+
+  if(value > 4300) 
+  {
+    inputString[index]=1;//calculating the number of dots and dashes in the character   
+    digitalWrite(ledBlue, HIGH);
+    delay(1000);// one second
+  }
+  else
+  {
+    inputString[index]=0; // calculating the number of spaces in dots and dashes
+    digitalWrite(ledBlue, LOW);
+    delay(1000);//one second ;
+  }
+  
+}
+
+
+bool SignalRead::strcomp(char a[], char b[])
+{
+ for (int i = 0; i<14; i++)
+    if(a[i] != b[i])
+      return false;
+
+ return true;
+}
+
+
+
+char SignalRead::getDefinition(char s[])
+{
+  if(strcomp(s,A))
+    return 'A';
+  else if (strcomp(s,B))
+    return 'B';
+  else if (strcomp(s,Cee))
+    return 'C';
+  else if (strcomp(s,D))
+    return 'D';
+  else if (strcomp(s,E))
+    return 'E';
+  else if (strcomp(s,F))
+    return 'F';
+  else if (strcomp(s,G))
+    return 'G';
+  else if (strcomp(s,H))
+    return 'H';
+  else if (strcomp(s,I))
+    return 'I';
+  else if (strcomp(s,J))
+    return 'J';
+  else if (strcomp(s,K))
+    return 'K';
+  else if (strcomp(s,L))
+    return 'L';
+  else if (strcomp(s,M))
+    return 'M';
+  else if (strcomp(s,eN))
+    return 'N';
+  else if (strcomp(s,O))
+    return 'O';
+  else if (strcomp(s,P))
+    return 'P';
+  else if (strcomp(s,Q))
+    return 'Q';
+  else if (strcomp(s,R))
+    return 'R';
+  else if (strcomp(s,S))
+    return 'S';
+  else if (strcomp(s,T))
+    return 'T';
+  else if (strcomp(s,U))
+    return 'U';
+  else if (strcomp(s,Vee))
+    return 'V';
+  else if (strcomp(s,W))
+    return 'W';
+  else if (strcomp(s,X))
+    return 'X';
+  else if (strcomp(s,Y))
+    return 'Y';
+  else if (strcomp(s,Zee))
+    return 'Z';
+  else if (strcomp(s,One))
+    return '1';
+  else if (strcomp(s,Two))
+    return '2';
+  else if (strcomp(s,Thr))
+    return '3';
+  else if (strcomp(s,Fou))
+    return '4';
+  else if (strcomp(s,Fiv))
+    return '5';
+  else if (strcomp(s,Six))
+    return '6';
+  else if (strcomp(s,Sev))
+    return '7';
+  else if (strcomp(s,Eig))
+    return '8';
+  else if (strcomp(s,Nin))
+    return '9';
+  else if (strcomp(s,Zer))
+    return '0';
+  else
+    return '_';
+}
+
